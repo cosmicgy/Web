23928dace9352f2998d6803d4622b2d0acc297dd
LAPTOP-02LK4BLI\gayou <cosmicgy@gmail.com>
2023-02-02 14:53:05 +0900

    ver5

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
-var elem = null;
+const todos = [
+  { id: 1, content: 'HTML', completed: true },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'JavaScript', completed: false }
+];
 
-console.log(elem.value);
-console.log(elem && elem.value);
+// 배열 => JSON 형식의 문자열
+const str = JSON.stringify(todos);
+console.log(typeof str, str);
+
+// JSON 형식의 문자열 => 배열
+const parsed = JSON.parse(str);
+console.log(typeof parsed, parsed);
